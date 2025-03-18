cdd88fe6ff2c3756a2b1f230fd4eb77fde666803
ThisIsSebi <shoff439@gmail.com>
2025-03-18 18:01:50 +0100

    Added more squares into the canvas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(0, 0, 0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+ 
+  if (mouseX >= 150 && mouseX <= 250 && mouseY >= 150 && mouseY <= 250) {
+    fill(0,255,255)
+    stroke(0,0,0)
+    rect(150, 150, 100, 100);
+  }
+
+  if(mouseX >= 250 && mouseX <= 350 && mouseY >= 150 && mouseY <= 250){
+    fill(0,255,0)
+    ellipse(300, 200, 100, 100);
 }
+
+  if (mouseX >=250 && mouseX <= 350 && mouseY >= 50 && mouseY <= 150) {
+    fill(255,0,0)
+    rect(250, 50, 100, 100);
+  }
+    
+  if (mouseX >= 350 && mouseX <= 450 && mouseY >= 150 && mouseY <= 250) {
+    fill(0, 0, 255);
+    rect(350, 150, 100, 100);
+  }
+  if(mouseX >= 250 && mouseX <= 350 && mouseY >= 250 && mouseY <= 350){
+    fill(255,0,255)
+    rect(250,250,100,100)
+  }
+    
+}
+
+// Colour change, shape change, triangle depending where mouseX or mouseY is
